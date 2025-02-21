d42b1ff68fcfe95a0a2c584ec4571ab0e6b5a4f3
Bill Ticehurst <billti@hotmail.com>
2025-02-21 09:33:19 -0500

    Sample trace

--- src/TODO.md
+++ src/TODO.md
@@ -1,42 +1,24 @@
 # TODO
 
-- Move all markup out of the HTML document
 - Load/run a script with movements and wire up to scrubber and controls
 - Draw the doublons firing and the measurements (lose / reload the qubit?)
 - Number the rows and columns
 - Draw a from/to line when qubits move
 - Fix the layout issues if initial size/scale is too large
-- Figure out the config format for layout and trace
 - Figure out status line and popup info for qubit hovering
 - Figure out how to show measurement results, qubit loss, and reloading
 - Figure out if/how to overlay noise or loss probabilities on qubits
 
-## Layout
-
-```json
-{
-    "regions": [
-        {
-            "title": "readout",
-            "rows": 6,
-            "columns": 16,
-            "startIdx": 0,
-            "position": "TODO"
-        },
-        {
-            "title": "storage",
-            "etc.": "TODO"
-        }
-    ]
-}
-```
 
 ## Trace
 
 ```json
 {
-    "mapping": "0 (0, 1)",
-    "steps": [
+    "metadata": {"name": "foo"}, // whatever
+    "qubits": [                  // index is qubit id, value is location
+        [3,0], [3,1], [3,2]      // etc..
+    ],
+    "steps": [ // id is just a (unique) label and doesn't matter
         {"id": 0, "ops": ["cx 0, 1", "move 3, (0, 2)", "etc."]}
     ]
 }
--- src/loader.ts
+++ src/loader.ts
@@ -7,10 +7,37 @@ const machineLayout = {
 
 export type MachineLayout = typeof machineLayout;
 
+export type Trace = {
+    metadata: any;
+    qubits: Array<[number, number]>;
+    steps: Array<{
+        id: string | number;
+        ops: Array<string>;
+    }>;
+}
+
 export function getMachine(): MachineLayout {
     return machineLayout;
 }
 
-export function getTrace() {
-
+export function getTrace() : Trace {
+    return {
+        metadata: {"name": "test-uranium-1a"},
+        qubits: [
+            [3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5],
+            [4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5],
+            [5, 0], [5, 1], [5, 2], [5, 3], [5, 4], [5, 5],
+            [6, 0], [6, 1], [6, 2], [6, 3], [6, 4], [6, 5],
+        ],
+        steps: [
+            {id: 1, ops: ["sx 0", "sx 1", "sx 2", "sx 3"]},
+            {id: 2, ops: ["move(2, 0) 0", "move(2, 1) 1","move(2, 2) 2", "move(2, 3) 3"  ]},
+            {id: 3, ops: ["cx 0, 1", "cx 2, 3"]},
+            {id: 4, ops: ["move(3, 0) 0", "move(3, 1) 1","move(3, 2) 2", "move(3, 3) 3"  ]},
+            {id: 5, ops: ["rz(0.7854) 0", "rz(0.7854) 2"]},
+            {id: 6, ops: ["move(1, 0) 1", "move(1, 1) 3"]},
+            {id: 7, ops: ["mz 1", "mz 3"]},
+            {id: 8, ops: ["reload(3, 1) 1", "reload(3, 3) 3"]},
+        ],
+    };
 }
