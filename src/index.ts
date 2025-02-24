d0339be32edc8a1999cb6f28a325681295113272
Bill Ticehurst <billti@hotmail.com>
2025-02-24 13:06:27 -0500

    Clean up dead code

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,23 @@ function render(container: HTMLElement, machineLayout: MachineLayout, trace: Tra
 
   scrubberControls.setNavHandler((step: number) => layout.gotoStep(step));
 
+  function setAppWidth() {
+    const newWidth = layout.width * layout.scale + 32;
+    newWidth > 600
+      ? (container.style.width = `${newWidth}px`)
+      : (container.style.width = "600px");
+  }
+
+  function onZoomIn() {
+    layout.zoomIn();
+    setAppWidth();
+  }
+
+  function onZoomOut() {
+    layout.zoomOut();
+    setAppWidth();
+  }
+
   // Wire up the controls
   container.tabIndex = 0;
   container.addEventListener("keydown", (e) => {
@@ -54,57 +71,6 @@ function render(container: HTMLElement, machineLayout: MachineLayout, trace: Tra
   const prev = document.querySelector( "[data-control='prev']") as SVGCircleElement;
   const zoomIn = document.querySelector( "[data-control='zoom-in']") as SVGCircleElement;
   const zoomOut = document.querySelector( "[data-control='zoom-out']") as SVGCircleElement;
-  const qubit = document.querySelector(".minpage-qubit") as SVGCircleElement;
-
-  function setAppWidth() {
-    const newWidth = layout.width * layout.scale + 32;
-    newWidth > 600
-      ? (container.style.width = `${newWidth}px`)
-      : (container.style.width = "600px");
-  }
-
-  function onNext() {
-    layout.renderGateOnQubit(2, "SX");
-    layout.renderGateOnQubit(14, "RZ", "2.35");
-    qubit
-      .animate(
-        [
-          { transform: "translate(5px, 65px)" },
-          { transform: "translate(15px, 45px)" },
-        ],
-        { duration: 250, fill: "forwards", easing: "ease" }
-      )
-      .finished.then((anim) => {
-        anim.commitStyles();
-        anim.cancel();
-      });
-  }
-
-  function onPrev() {
-    layout.clearGates();
-    qubit
-      .animate(
-        [
-          { transform: "translate(15px, 45px)" },
-          { transform: "translate(5px, 65px)" },
-        ],
-        { duration: 250, fill: "forwards", easing: "ease" }
-      )
-      .finished.then((anim) => {
-        anim.commitStyles();
-        anim.cancel();
-      });
-  }
-
-  function onZoomIn() {
-    layout.zoomIn();
-    setAppWidth();
-  }
-
-  function onZoomOut() {
-    layout.zoomOut();
-    setAppWidth();
-  }
 
   next.addEventListener("click", () => scrubberControls.next());
   prev.addEventListener("click", () => scrubberControls.prev());
